784b31efd145e37cd82026f8c1b9d3795821261f
paljiv <ivan.paljevic@hotmail.com>
2024-12-17 16:41:56 +0100

    add name and icon

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -1,3 +1,19 @@
 public class Giocatore {
-    
+
+    String name;
+
+    String icon;
+
+    public Giocatore(String name, String icon) {
+        this.name = name;
+        this.icon = icon != null ? "" + icon.charAt(0) : "X";
+    }
+
+    public String getIcon() {
+        return this.icon;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
