4e661247d3a3be8009a39ade090a1a6b4f1b6c92
IsmaelTrentin <pandux66@gmail.com>
2025-01-03 17:58:55 +0100

    update: win count

--- src/Partita.java
+++ src/Partita.java
@@ -3,7 +3,7 @@
  *
  * @author Ivan Paljevic
  * @author Ismael Trentin
- * @version 2024.12.20
+ * @version 2025.01.03
  */
 public class Partita {
 
@@ -13,6 +13,8 @@ public class Partita {
 
     Giocatore player1;
     Giocatore player2;
+
+    int[] playerWins = new int[2];
 
     public Partita() {
         this.matches = new Match[0];
@@ -38,22 +40,25 @@ public class Partita {
     }
 
     Giocatore getWinner() {
-        int p1Wins = 0;
-        int p2Wins = 0;
-
-        for (Match match : this.matches) {
-            Giocatore winner = match.getWinner();
-            if (winner == this.player1) {
-                p1Wins++;
-            } else if (winner == this.player2) {
-                p2Wins++;
-            }
-        }
-
-        return p1Wins > p2Wins ? this.player1 : this.player2;
+        // int p1Wins = 0;
+        // int p2Wins = 0;
+        //
+        // for (Match match : this.matches) {
+        // Giocatore winner = match.getWinner();
+        // if (winner == this.player1) {
+        // p1Wins++;
+        // } else if (winner == this.player2) {
+        // p2Wins++;
+        // }
+        // }
+        //
+        // return p1Wins > p2Wins ? this.player1 : this.player2;
+        return this.playerWins[0] > this.playerWins[1] ? this.player1 : this.player2;
     }
 
     void start(GestioneInput input) {
+        Ansi.clearScreen();
+
         // player 1
         String name = input.askString("nome giocatore1: ");
         Ansi.clearScreen();
@@ -75,6 +80,7 @@ public class Partita {
         Ansi.clearScreen();
         this.player2 = new Giocatore(name, icon);
 
+        // number of matches
         int nMatches = input.askNumberOfMatches("numero di match: ");
         Ansi.clearScreen();
         matches = new Match[nMatches];
@@ -86,26 +92,47 @@ public class Partita {
             this.matches[i] = match;
 
             match.start(input); // blocking
-            Giocatore winner = match.getWinner();
+            Giocatore matchWinner = match.getWinner();
 
-            if (match.hasEnded() && winner == null) {
-                input.askString("nessun vincitore! premi enter per rigiocare il match");
+            if (match.hasEnded() && matchWinner == null) {
+                Ansi.clearScreen();
 
-                // step back one iteration and then
-                // restart with the same index.
+                System.out.println("pareggio!");
+                System.out.println(match.gridToString());
+                System.out.print("premi un qualsiasi tasto per rigiocare o q per uscire ");
+                int key = input.read(); // blocking
+                if (key == 'q') {
+                    Ansi.clearScreen();
+                    System.out.println("partita terminata");
+                    return;
+                }
+
+                // step back one iteration to repeat
+                // the last game since it was a draw.
                 i -= 1;
                 continue;
             }
 
+            if (matchWinner == this.player1) {
+                this.playerWins[0]++;
+            } else if (matchWinner == this.player2) {
+                this.playerWins[1]++;
+            }
+
             Ansi.clearScreen();
+            System.out.printf("match %d vinto da %s!\n", i + 1, matchWinner.getName());
             System.out.println(match.gridToString());
-            System.out.printf("match %d vinto da %s!\n", i + 1, winner.getName());
+            if (i + 1 != matches.length) {
+                System.out.println("premi un qualsiasi tasto per continuare ");
+                input.read();
+            }
         }
 
-        Giocatore matchWinner = this.getWinner();
+        Giocatore gameWinner = this.getWinner();
 
         System.out.println();
-        System.out.printf(" ⭐️ Il vincitore della partita e': %s %s! ⭐️\n", matchWinner.getIcon(),
-                matchWinner.getName());
+        System.out.printf("Vittorie %s: %d\r\n", this.player1.getName(), this.playerWins[0]);
+        System.out.printf("Vittorie %s: %d\r\n", this.player2.getName(), this.playerWins[1]);
+        System.out.printf(" ⭐️ Il vincitore della partita e': %s! ⭐️\n", gameWinner.getName());
     }
 }
