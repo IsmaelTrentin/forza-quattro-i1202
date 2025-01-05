ca4cd3c2e08ac12b9b6038533af01a28adfcaf48
IsmaelTrentin <pandux66@gmail.com>
2025-01-05 23:36:18 +0100

    feat: start menu

--- src/ForzaQuattro.java
+++ src/ForzaQuattro.java
@@ -5,8 +5,10 @@ public class ForzaQuattro {
 
     public static void main(String[] args) {
         GestioneInput input = new GestioneInput();
+        StartMenu menu = new StartMenu();
         Partita gm = new Partita();
 
+        menu.show(input); // blocking
         gm.start(input);
 
         input.rilascia();
---
+++ src/StartMenu.java
@@ -0,0 +1,37 @@
+public class StartMenu {
+
+  void show(GestioneInput input) {
+    Ansi.clearScreen();
+
+    String[] headerLines = {
+        "     /$$$$$$$$/$$$$$$  /$$$$$$$  /$$$$$$$$  /$$$$$$         /$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$$$/$$$$$$$$/$$$$$$$   /$$$$$$ ",
+        "    | $$_____/$$__  $$| $$__  $$|_____ $$  /$$__  $$       /$$__  $$| $$  | $$ /$$__  $$|__  $$__/__  $$__/ $$__  $$ /$$__  $$",
+        "    | $$    | $$  \\ $$| $$  \\ $$     /$$/ | $$  \\ $$      | $$  \\ $$| $$  | $$| $$  \\ $$   | $$     | $$  | $$  \\ $$| $$  \\ $$",
+        "    | $$$$$ | $$  | $$| $$$$$$$/    /$$/  | $$$$$$$$      | $$  | $$| $$  | $$| $$$$$$$$   | $$     | $$  | $$$$$$$/| $$  | $$",
+        "    | $$__/ | $$  | $$| $$__  $$   /$$/   | $$__  $$      | $$  | $$| $$  | $$| $$__  $$   | $$     | $$  | $$__  $$| $$  | $$",
+        "    | $$    | $$  | $$| $$  \\ $$  /$$/    | $$  | $$      | $$/$$ $$| $$  | $$| $$  | $$   | $$     | $$  | $$  \\ $$| $$  | $$",
+        "    | $$    |  $$$$$$/| $$  | $$ /$$$$$$$$| $$  | $$      |  $$$$$$/|  $$$$$$/| $$  | $$   | $$     | $$  | $$  | $$|  $$$$$$/",
+        "    |__/     \\______/ |__/  |__/|________/|__/  |__/       \\____ $$$ \\______/ |__/  |__/   |__/     |__/  |__/  |__/ \\______/ ",
+        "                                                                \\__/                                                          "
+    };
+
+    System.out.println();
+    System.out.println();
+    for (String line : headerLines) {
+      System.out.println(Ansi.red(line));
+    }
+
+    System.out.println();
+    System.out.println();
+    System.out.print(Ansi.cyan("    -> "));
+    System.out.print(Ansi.yellow(Ansi.bold("premi un tasto per giocare")));
+    System.out.print(Ansi.cyan(" <-"));
+    System.out.println();
+    System.out.println();
+    System.out.print(Ansi.fg(Ansi.italic("    //    Ivan Paljevic & Ismael Trentin    //"), 90, 92, 90));
+    System.out.println();
+    System.out.println();
+
+    input.read();
+  }
+}
